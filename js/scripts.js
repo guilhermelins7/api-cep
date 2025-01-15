c9ba44d71c665d3e2bb58330238b3e5bd6a0af0c
guilhermelins7 <guilherme7lins@gmail.com>
2025-01-14 22:28:41 -0300

    feat: adicionar criação de componentes de endereço ao histórico
    Agora, ao realizar uma consulta de CEP, um novo componente de endereço é criado e adicionado à seção de histórico.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,7 @@
 
 const cep = document.getElementById("CEP");
 const pesquisar = document.getElementById("pesquisar");
-const estado = document.getElementById("estado");
-const localidade = document.getElementById("localidade");
-const logradouro = document.getElementById("logradouro");
+const historicoLista = document.getElementById("historico-lista");
 
 function getCep(cep) {
     return cep.value;
@@ -17,16 +15,28 @@ pesquisar.addEventListener("click", async () => {
         // const resposta = await fetch(`https://viacep.com.br/ws/11500260/json/`);
 
 
-        if (!resposta.ok) throw new erro();
+        if (!resposta.ok) throw new Error("Falha ao buscar CEP");
 
         // Desserialização JSON
         const dadosJSON = await resposta.json();
+        
+        // Criar novo componente:
+        const novoEndereco = document.createElement("div");
+        novoEndereco.classList.add("endereco");
+        novoEndereco.innerHTML = `
+            <h2 class="estado">${dadosJSON.uf}</h2>
+            <div class="detalhes">
+                <div>
+                    <h3 class="localidade">${dadosJSON.localidade}</h3>
+                    <p class="logradoura">${dadosJSON.logradouro}, ${dadosJSON.bairro}</p>
+                </div>
+                <p class="cep-buscado">CEP: ${getCep(cep)}</p>
+            </div>
+        `;
 
-        estado.textContent = `${dadosJSON.uf}`;
-        localidade.textContent = `${dadosJSON.localidade}`;
-        logradouro.textContent = `${dadosJSON.logradouro}, ${dadosJSON.bairro}`
+        historicoLista.appendChild(novoEndereco);
     }
     catch(erro) {   
-        estado.textContent = "Falha ao buscar CEP."
+        alert(`Erro: ${erro.message}`);
     }
 })
